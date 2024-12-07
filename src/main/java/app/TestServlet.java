a98f11899cf20fa900aefe93d43fa9b022cf1069
Daniil4639 <Danmarchenko2004@yandex.ru>
2024-12-07 23:52:46 +0300

    adding Tomcat part

---
+++ src/main/java/app/TestServlet.java
@@ -0,0 +1,20 @@
+package app;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/test_servlet")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.getWriter().write("Test servlet container is working!");
+    }
+}
