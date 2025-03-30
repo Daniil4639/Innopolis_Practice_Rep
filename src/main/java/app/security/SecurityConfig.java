26a4d16499efb46f06b4a8677408a574129bc503
Daniil4639 <Danmarchenko2004@yandex.ru>
2025-03-30 15:07:39 +0300

    spring basic auth realization

--- src/main/java/app/security/SecurityConfig.java
+++ src/main/java/app/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -37,25 +38,28 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain configSecurityChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configSecurityChain(HttpSecurity http,TasksBasicAuthEntryPoint entryPoint,
+                                                   LoggingFilter filter) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .addFilterBefore(filter, BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/users**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/tasks").hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/tasks**").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                ).formLogin(form -> form
-                        .loginPage("/custom-login.html")
-                        .loginProcessingUrl("/perform_login")
-                        .defaultSuccessUrl("/tasks",true)
-                        .failureUrl("/custom-login.html?error=true")
-                        .permitAll()
-                ).logout(logout->logout
-                        .logoutUrl("/perform_logout")
-                        .logoutSuccessUrl("/custom-login?logout=true")
-                        .permitAll()
-                );
+//                ).formLogin(form -> form
+//                        .loginPage("/custom-login.html")
+//                        .loginProcessingUrl("/perform_login")
+//                        .defaultSuccessUrl("/tasks",true)
+//                        .failureUrl("/custom-login.html?error=true")
+//                        .permitAll()
+//                ).logout(logout->logout
+//                        .logoutUrl("/perform_logout")
+//                        .logoutSuccessUrl("/custom-login?logout=true")
+//                        .permitAll()
+//                );
+                ).httpBasic(basic -> basic.authenticationEntryPoint(entryPoint));
 
         return http.build();
     }
